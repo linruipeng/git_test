caa6ddc938af5214551251e98bc809dc6802153d
LinRuiPeng <1769032783@qq.com>
2022-10-24 22:07:44 +0800

    林瑞鹏首次提交

---
+++ src/main/java/com/demo1/GitTest1.java
@@ -0,0 +1,20 @@
+package com.demo1;
+
+/**
+ * @author linruipeng
+ * @version 1.0
+ * @ClassName GitTest1
+ * @date 2022/10/24 0024 22:03
+ */
+public class GitTest1 {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git2");
+        System.out.println("hello git3");
+        System.out.println("hello git4");
+        System.out.println("master test");
+        System.out.println("hot-fix test");
+        System.out.println("push test");
+        System.out.println("pull test");
+    }
+}
