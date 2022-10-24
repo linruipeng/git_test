7b06a22c0befeb8fc925fe394b5e283cd5976ed8
LinRuiPeng <1769032783@qq.com>
2022-10-24 23:08:29 +0800

    m1

--- src/main/java/com/demo1/GitTest1.java
+++ src/main/java/com/demo1/GitTest1.java
@@ -8,13 +8,10 @@ package com.demo1;
  */
 public class GitTest1 {
     public static void main(String[] args) {
-        System.out.println("hello git");
+        System.out.println("hello git1");
         System.out.println("hello git2");
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("master test");
-        System.out.println("hot-fix test");
-        System.out.println("push test");
-        System.out.println("pull test");
+
     }
 }
